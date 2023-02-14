0e29c5f3ec0bbdcb0a06c4fee559a9af5baf593a
EC2 Default User <ec2-user@ip-172-31-73-62.ec2.internal>
2023-02-14 21:45:51 +0000

    adding my own fork to webhook

--- Jenkins/pipelines/Webhook.groovy
+++ Jenkins/pipelines/Webhook.groovy
@@ -13,7 +13,7 @@ pipeline {
     stages {
         stage('Do job stage') {
             steps {
-                git branch: params.branch, url: 'https://github.com/naturalett/webhook-test.git'
+                git branch: params.branch, url: 'https://github.com/continousintegeration/continuous-integration-tutorial.git'
             }
         }
     }
